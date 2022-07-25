c8b20315e5e4edf96f371aef202e37fd1924aa4f
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2022-07-25 14:07:21 +0200

    aa

---
+++ paginaPokemon/js/pokemons.js
@@ -0,0 +1,37 @@
+//Traer imagen de pokemon
+var getPokemonImage = async(url) => {
+    try{
+        var respuesta = await fetch(url);
+        var datos = await respuesta.json();
+        
+        var img = document.createElement('img');
+        img.src = datos.sprites.front_default;
+        document.body.appendChild(img);
+        
+    } catch{
+        console.log(error);
+    }
+}
+
+
+//Traer la lista de los pokemon y mostrar sus nombres
+var url = "https://pokeapi.co/api/v2/pokemon";
+var getPokemons = async(url) => {
+    try{
+        var respuesta = await fetch(url);
+        var datos = await respuesta.json();
+        var lista = datos.results;
+        
+        for (var pokemon of lista) {
+            var frase = document.createElement('p');
+            frase.innerText = pokemon.name;
+            document.body.appendChild(frase);
+
+            await getPokemonImage(pokemon.url)
+        }
+    } catch{
+        console.log(error);
+    }
+}
+
+getPokemons(url);
