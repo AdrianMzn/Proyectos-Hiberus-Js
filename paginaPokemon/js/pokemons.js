ff8b5026d095511e1993425267948703e64674f8
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2022-07-26 00:01:40 +0200

    comentarios ok

--- paginaPokemon/js/pokemons.js
+++ paginaPokemon/js/pokemons.js
@@ -1,7 +1,7 @@
 let pokemons = new Array();
 var pokemonsLeidos = false;
 
-//Traemos la primera generacion (151 pokemons)
+//Traemos la primera generacion (151 pokemons) auqnue podriamos traer hasta 1000000
 let url = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
 
 let getPokemonInfo = async(url) => {
@@ -14,40 +14,38 @@ let getPokemonInfo = async(url) => {
     }
 }
 
-let getPokemons = async(url) => {
+// Funcion que hace peticion al servidor para pillar la informacion de los pokemons, solo se llama una vez al cargar la web
+let getPokemons = async() => {
     try{
         pokemons = [];
         let respuesta = await fetch(url);
         let datos = await respuesta.json();
         let lista = datos.results;
 
-        console.log(datos);
         for (let pokemon of lista) {
             await getPokemonInfo(pokemon.url)
         }
+
+        await selectPokemons();
         
     } catch{
         console.log(error);
     }
 }
 
+// Funcion usada para actualizar el select de los nombres de los pokemons, se llamará cuando se
+//  seleccione un tipo de pokemon para solo mostrar los pokemons de ese tipo en el select
 let selectPokemons = async() => {
 
-    if ( !pokemonsLeidos ){
-        await getPokemons(url)
-        pokemonsLeidos = true;
-    }
-    
-    
-    
     document.querySelector('#formularioTarjeta').innerHTML += `<img id="opacity" src="../img/pokemons.jpg" alt="pokemon">`
-    
 
     let tipoBuscado = document.getElementById("pokemonTipo").value;
     let nombreBuscado = document.getElementById("pokemon").value;
 
     document.querySelector('#pokemon').innerHTML = '<option value="default">Selecciona un Pokemon</option>';
     
+    // Cuando actualizamos el select nos aseguramos de mantener seleccionado el pokemon 
+    // que ya habia sido seleccionado por el usuario
     pokemons.forEach((poke) => {
 
         if (tipoBuscado == "default" || ( poke.types.length == 2 && ( poke.types[0].type.name == tipoBuscado || poke.types[1].type.name == tipoBuscado) 
@@ -64,18 +62,19 @@ let selectPokemons = async() => {
     })
 }
 
+// Funcion que muestra las carpetas de los pokemons que cumplen las condiciones del formulario
 let pintarPokemons = async() =>{
 
     let nombreBuscado = document.getElementById("pokemon").value;
     let tipoBuscado = document.getElementById("pokemonTipo").value;
     await selectPokemons();
     
+    // Filtro por tipo
     let vectorFilterTipo = pokemons.filter( item => tipoBuscado == "default" ||
         ( item.types.length == 2 && ( item.types[0].type.name == tipoBuscado || item.types[1].type.name == tipoBuscado) 
         || item.types.length == 1 && item.types[0].type.name == tipoBuscado ));
-
-    console.log(vectorFilterTipo)
     
+    //Filtro por nombre
     let vectorFilter = vectorFilterTipo.filter(item => item.name.includes(nombreBuscado) || nombreBuscado == "default");
 
     // Si el vector de pokemons despues de filtrar por nombre es vacio pero tenemos seleccionado un tipo entonces devolvemos los pokemons de ese tipo
@@ -83,7 +82,7 @@ let pintarPokemons = async() =>{
         vectorFilter = vectorFilterTipo;
     }
 
-    console.log(vectorFilter)
+    // Pintamos una a una las tarjetas de los pokemons que cumplen los filtros
     document.querySelector('#formularioTarjeta').innerHTML = '';
     vectorFilter.forEach((poke) => {
         document.querySelector('#formularioTarjeta').innerHTML += `<div id="tarjetas"><p>${poke.name}</p><img src="${poke.sprites.front_shiny}" alt=""></div>`
