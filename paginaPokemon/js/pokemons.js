5fcbace8dfc821206e8ca7461cdc9c3350cff182
AinhoaSalo <94933683+AinhoaSalo@users.noreply.github.com>
2022-07-25 18:20:37 +0200

    cambios

--- paginaPokemon/js/pokemons.js
+++ paginaPokemon/js/pokemons.js
@@ -13,6 +13,7 @@ let getPokemonInfo = async(url) => {
 let url = "https://pokeapi.co/api/v2/pokemon";
 let getPokemons = async(url) => {
     try{
+        pokemons = [];
         let respuesta = await fetch(url);
         let datos = await respuesta.json();
         let lista = datos.results;
@@ -30,16 +31,22 @@ let selectPokemons = async() => {
     await getPokemons(url)
     pokemons.forEach((poke) => {
         document.querySelector('#pokemon').innerHTML += `<option value=${poke.name}>${poke.name}</option>`;
+        // document.querySelector('#formularioTarjeta').innerHTML += `<div id="tarjetas"><p>${poke.name}</p><img src="${poke.sprites.front_shiny}" alt=""></div>`
+    })
+}
+
+
+
+let pintarPokemons = async() =>{
+    debugger
+    await getPokemons(url)
+
+    var nombreBuscado = document.getElementById("pokemon").value;
+    // var tipoBuscado = document.getElementById("tipoPokemon").value;
+    var nuevoVector = pokemons.filter(item => item.name.includes(nombreBuscado));
+    nuevoVector.forEach((poke) => {
         document.querySelector('#formularioTarjeta').innerHTML += `<div id="tarjetas"><p>${poke.name}</p><img src="${poke.sprites.front_shiny}" alt=""></div>`
     })
 }
 
-//document.querySelector('#pokemon').onchange = pintarPokemons;
-
-// let pintarPokemons = async() =>{
-//     await getPokemons(url)
-//     pokemons.forEach((poke) => {
-//         document.querySelector('#formularioTarjeta').innerHTML += `<div id="tarjetas"><p>${poke.name}</p><img src="${poke.sprites.front_shiny}" alt=""></div>`
-//     })
-// }
-
+document.querySelector('#pokemon').onchange = pintarPokemons;
